cc30f141fdd45752f29a63e1de77641a91f9053f
Jesica Velazquez <jesica.velazquez@ibm.com>
2021-01-07 16:45:51 -0300

    ultimas modificaciones

---
+++ README.md
@@ -0,0 +1,25 @@
+# Delilah Resto Acamica 🍱
+
+## Descripción general
+
+Crearás el backend para un sistema de pedidos online para un restaurante poniendo en funcionamiento las partes necesarias para montar una REST API que permita realizar operaciones CRUD sobre una estructura de datos.
+
+## Tecnologias utilizadas
+
+MySQL
+SQL
+Express
+NODE.JS
+Postman
+
+## Para correrlo 🚀
+
+Ejecutar el archivo create_tables.sql
+Ejecutar el archivo inserts.sql
+Correr el proyecto con el script start o desde la terminal nodemon server.js
+
+Ejemplo de endpoint
+
+http://localhost:3000/login
+usuario: admin
+contraseña: 12345
---
+++ create_tables.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE delilah_resto;
+
+CREATE TABLE producto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    descripcion VARCHAR (60) NOT NULL,
+    precio INT NOT NULL
+);
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR (60) NOT NULL,
+    contrasena VARCHAR (60) NOT NULL,
+    nombre_apellido VARCHAR (60) NOT NULL,
+    e_mail VARCHAR (60) NOT NULL,
+    telefono VARCHAR (60) NOT NULL,
+    direccion VARCHAR (60) NOT NULL,
+    es_admin VARCHAR (1) DEFAULT 'F' NOT NULL
+);
+
+
+CREATE TABLE pedido(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    estado VARCHAR (3) NOT NULL,
+    forma_de_pago VARCHAR (1) NOT NULL,
+    id_usuario INT NOT NULL,
+    fecha_hora DATE NOT NULL
+);
+
+CREATE TABLE detalle_pedido(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_pedido INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad_platos INT NOT NULL
+);
